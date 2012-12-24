7b4e1aefd7b38cc115a27f5f4f005849ab8e1630
Ugo Lattanzi <imperugo@gmail.com>
2012-12-24 16:33:21 +0100

    added default post to setup procedure

--- src/Dexter.Services.Implmentation/SetupService.cs
+++ src/Dexter.Services.Implmentation/SetupService.cs
@@ -11,10 +11,15 @@
 // Dexter is hosted to Github at https://github.com/imperugo/Dexter-Blog-Engine
 // For any question contact info@dexterblogengine.com
 // ////////////////////////////////////////////////////////////////////////////////////////////////
+
 #endregion
 
 namespace Dexter.Services.Implmentation
 {
+	using System;
+	using System.IO;
+	using System.Web.Security;
+
 	using Dexter.Data;
 	using Dexter.Entities;
 
@@ -24,13 +29,16 @@ namespace Dexter.Services.Implmentation
 
 		private readonly IConfigurationDataService configurationDataService;
 
+		private readonly IPostDataService postDataService;
+
 		#endregion
 
 		#region Constructors and Destructors
 
-		public SetupService(IConfigurationDataService configurationDataService)
+		public SetupService(IConfigurationDataService configurationDataService, IPostDataService postDataService)
 		{
 			this.configurationDataService = configurationDataService;
+			this.postDataService = postDataService;
 		}
 
 		#endregion
@@ -57,10 +65,25 @@ namespace Dexter.Services.Implmentation
 
 			this.configurationDataService.SaveConfiguration(configuration);
 
-			// Membership.CreateUser(item.AdminUsername, item.AdminPassword, item.Email.Address);
-			// Roles.CreateRole("Administrator");
+			//Creating roles
+			Membership.CreateUser(item.AdminUsername, item.AdminPassword, item.Email.Address);
+			Roles.CreateRole("Administrator");
 
-			// Roles.AddUserToRole(item.AdminUsername, "Administrator");
+			Roles.AddUserToRole(item.AdminUsername, "Administrator");
+
+			//Creating default post
+			PostDto defaultPost = new PostDto();
+
+			string defaultPostPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data/Setup/defaultPost.dxt");
+
+			defaultPost.Title = "Welcome to Dexter!";
+			defaultPost.Tags = new[] { "Dexter" };
+			defaultPost.Categories = new[] { "Various" };
+			defaultPost.Content = File.ReadAllText(defaultPostPath).Replace("[SiteDomain]", item.SiteDomain.Host);
+			defaultPost.Status = ItemStatus.Published;
+			defaultPost.PublishAt = DateTimeOffset.Now.AsMinutes();
+
+			this.postDataService.SaveOrUpdate(defaultPost);
 		}
 
 		#endregion
