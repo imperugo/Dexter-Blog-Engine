cca3f2e069d9b5424d17a51c720e66e71c7fe5eb
Ugo Lattanzi <imperugo@gmail.com>
2013-05-05 11:24:31 +0200

    added featured image to item

--- src/Dexter.Data.Raven/Domain/Item.cs
+++ src/Dexter.Data.Raven/Domain/Item.cs
@@ -5,12 +5,13 @@
 // Website:		http://dexterblogengine.com/
 // Authors:		http://dexterblogengine.com/aboutus
 // Created:		2012/10/27
-// Last edit:	2013/01/20
+// Last edit:	2013/05/05
 // License:		New BSD License (BSD)
 // For updated news and information please visit http://dexterblogengine.com/
 // Dexter is hosted to Github at https://github.com/imperugo/Dexter-Blog-Engine
 // For any question contact info@dexterblogengine.com
 // ////////////////////////////////////////////////////////////////////////////////////////////////
+
 #endregion
 
 namespace Dexter.Data.Raven.Domain
@@ -22,25 +23,27 @@ namespace Dexter.Data.Raven.Domain
 
 	public abstract class Item : EntityBase<string>
 	{
-		#region Public Properties
+		public string Title { get; set; }
+
+		public string Slug { get; set; }
+
+		public DateTimeOffset PublishAt { get; set; }
+
+		public string Excerpt { get; set; }
+
+		public string Content { get; set; }
+
+		public string SearchContent { get; set; }
+
+		public ItemStatus Status { get; set; }
+
+		public Uri FeaturedImage { get; set; }
+
+		public string Author { get; set; }
 
 		public bool AllowComments { get; set; }
 
 		public string CommentsId { get; set; }
-
-		public string Content { get; set; }
-
-		public string Excerpt { get; set; }
-
-		public DateTimeOffset PublishAt { get; set; }
-
-		public string SearchContent { get; set; }
-
-		public string Slug { get; set; }
-
-		public ItemStatus Status { get; set; }
-
-		public string Title { get; set; }
 
 		public int TotalComments { get; set; }
 
@@ -48,10 +51,6 @@ namespace Dexter.Data.Raven.Domain
 
 		public string TrackbacksId { get; set; }
 
-		public string Author { get; set; }
-
 		public ICollection<string> Tags { get; set; }
-
-		#endregion
 	}
 }
